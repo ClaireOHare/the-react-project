f855d4fa5c82baf13208819c469e34ec913b1f68
Claire O'Hare <claire.ohare@bbc.co.uk>
2020-06-28 22:16:46 +0100

    Added button to index and fixed css errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PrimaryButton from './components/button'
 
-const App = () => <div>Hello World</div>
+
+const App = () => <PrimaryButton>Hello World</PrimaryButton>
 
 ReactDOM.render(<App />, document.querySelector("#root"))
+
