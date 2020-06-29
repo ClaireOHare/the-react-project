7ef1cccdf3a48873830cb3736a810a37d94d6d91
Claire O'Hare <claire.ohare@bbc.co.uk>
2020-06-29 13:22:05 +0100

    Refactored for a Button componenet that the Primary Button could inherit

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,17 @@
 import styled from 'styled-components'
 
-const PrimaryButton = styled.button `
+const Button = styled.button `
+    padding: 12px 24px;
+    fontsize: 1rem;
+    border-radius: 2px;
+    min-width: 100px;
+    cursor: pointer;
+    font-family: 'Roberto Mono', monospace;
+`
+
+const PrimaryButton = styled(Button) `
     background-color: red;
     border: none;
     color: white;
-    padding: 12px 24px;
-    fontsize: 1rem;
 `
 export default PrimaryButton
