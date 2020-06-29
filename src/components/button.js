c4b41790982110481c15172f6817fe731cc7f29a
Claire O'Hare <claire.ohare@bbc.co.uk>
2020-06-29 13:51:35 +0100

    Added Secondary and Tertiary button sytles, and nano sand font

--- src/components/button.js
+++ src/components/button.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+const PrimaryColour = '#E08524'
+const SecondaryColour = '#B06617'
+const TertiaryColour = '#F2830C'
+
 const Button = styled.button `
     padding: 12px 24px;
     fontsize: 1rem;
@@ -7,11 +11,18 @@ const Button = styled.button `
     min-width: 100px;
     cursor: pointer;
     font-family: 'Roberto Mono', monospace;
-`
-
-const PrimaryButton = styled(Button) `
-    background-color: red;
     border: none;
     color: white;
 `
-export default PrimaryButton
+
+const PrimaryButton = styled(Button) `
+    background-color: ${PrimaryColour}
+`
+export const SecondaryButton = styled(Button) `
+    background-color: ${SecondaryColour}
+`
+
+export const TertiaryButton = styled(Button) `
+    background-color: ${TertiaryColour}
+`
+export default 
