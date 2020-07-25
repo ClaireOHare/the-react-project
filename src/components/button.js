02c2526828f476a29db1804911adffdbdfc160a5
Claire O'Hare <claire.ohare@bbc.co.uk>
2020-07-25 10:56:33 +0100

    Update to buttons and adding prettier

--- src/components/button.js
+++ src/components/button.js
@@ -1,34 +1,52 @@
 import styled from 'styled-components'
-import {defaultTheme, typeScale} from "../utils"
+import { defaultTheme, typeScale } from "../utils"
 
 const PrimaryColour = '#E08524'
 const SecondaryColour = '#B06617'
 const TertiaryColour = '#F2830C'
 
-const Button = styled.button `
-    padding: 12px 24px;
-    fontsize: ${typeScale.paragraph};
-    border-radius: 2px;
-    min-width: 100px;
-    cursor: pointer;
-    font-family: 'Roberto Mono', monospace;
-    border: none;
-    transition: background-color 0.2s linear, color 0.2s linear;
+const Button = styled.button`
+padding: 12px 24px;
+fontsize: ${typeScale.paragraph};
+border-radius: 2px;
+min-width: 100px;
+cursor: pointer;
+font-family: 'Roberto Mono', monospace;
+border: none;
+transition: background-color 0.2s linear, color 0.2s linear;
 
-    &:hover {
-        background-color: ${defaultTheme.primaryHoverColor};
-        color: ${defaultTheme.textColorOnPrimary};
-      }
+&:hover {
+background-color: ${defaultTheme.primaryHoverColor};
+color: ${defaultTheme.textColorOnPrimary};
+}
+
+&:focus {
+outline: 3px solid ${defaultTheme.primaryHoverColor};
+outline-offset: 2px;
+}
+
+&:active {
+background-color: ${defaultTheme.primaryActiveColor};
+border-color: ${defaultTheme.primaryActiveColor};
+color: ${defaultTheme.textColorOnPrimary};
+}
+
+&:disabled {
+cursor: not-allowed;
+background: none;
+color: ${defaultTheme.disabled};
+border: none;
+}
 `
 
-const PrimaryButton = styled(Button) `
-    background-color: ${PrimaryColour}
+const PrimaryButton = styled(Button)`
+background-color: ${PrimaryColour}
 `
-export const SecondaryButton = styled(Button) `
-    background-color: ${SecondaryColour}
+export const SecondaryButton = styled(Button)`
+background-color: ${SecondaryColour}
 `
 
-export const TertiaryButton = styled(Button) `
-    background-color: ${TertiaryColour}
+export const TertiaryButton = styled(Button)`
+background-color: ${TertiaryColour}
 `
 export default PrimaryButton
